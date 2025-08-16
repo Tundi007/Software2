49d61e74ac84ed6834662ee11f9da71e1cbe2b30
Amir Reza Qasemi <83636447+AR-Q@users.noreply.github.com>
2025-08-16 12:34:49 +0330

    fix cors

--- API-Gatewat/API-Gatewat/Program.cs
+++ API-Gatewat/API-Gatewat/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000") // Your Next.js frontend URL
+        policy.WithOrigins("https://skydock.liara.run") // Your Next.js frontend URL
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
